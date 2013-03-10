96ad447812c354c0b202600a564afad92f236a06
Ed Finkler <coj@funkatron.com>
2013-03-10 18:45:02 -0400

    add in more expected config vars to sample

--- settings/sample.py
+++ settings/sample.py
@@ -3,3 +3,10 @@
 # > export NCS_NOTIFICATION_SETTINGS=/var/www/ncs-notifications/settings/sample.py; python run.py
 
 DEBUG = True
+
+PM_API_KEY = 'MAILCHIMP_KEY'
+PM_LIST_ID = u'MAILCHIMP_LIST_ID'
+MD_API_KEY = 'MANDRILL_KEY'
+
+DEFAULT_FROM_EMAIL = 'postmaster@from.domain'
+DEFAULT_FROM_NAME = 'Postmaster'
